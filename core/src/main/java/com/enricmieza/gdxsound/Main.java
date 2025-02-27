08655db4620cdad1e1a08cd360666520c153d8a5
Enric Mieza <emieza@xtec.cat>
2025-02-27 23:44:58 +0100

    start/stop onTouch

--- core/src/main/java/com/enricmieza/gdxsound/Main.java
+++ core/src/main/java/com/enricmieza/gdxsound/Main.java
@@ -2,7 +2,6 @@ package com.enricmieza.gdxsound;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -13,11 +12,14 @@ import com.badlogic.gdx.audio.AudioDevice;
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
-
     float freq = 440.0f;        // en Hz
     float sampleRate = 44100;   // 44.1 kHz - qualitat CD
+    float step = 0.1f;          // fragment minim de so
+    boolean sona = true;
+    boolean running = true;
+    AudioDevice audioDevice;
 
-    public static float[] generateSineWave(float frequency, float sampleRate, int durationInSeconds) {
+    public static float[] generaSinusoide(float frequency, float sampleRate, float durationInSeconds) {
         int numSamples = (int) (sampleRate * durationInSeconds);
         float[] samples = new float[numSamples];
         for (int i = 0; i < numSamples; i++) {
@@ -27,15 +29,26 @@ public class Main extends ApplicationAdapter {
         return samples;
     }
 
-    AudioDevice audioDevice;
+    public static float[] generaSilenci(float sampleRate, float durationInSeconds) {
+        int numSamples = (int) (sampleRate * durationInSeconds);
+        float[] samples = new float[numSamples];
+        for (int i = 0; i < numSamples; i++) {
+            samples[i] = (float) 0.0f;
+        }
+        return samples;
+    }
+
     Thread soundThread = new Thread(() -> {
-        float sampleRate = 44100; // 44.1 kHz
-        float frequency = 440; // 440 Hz (La4)
-        int durationInSeconds = 2; // 2 segons
+        float[] sineWave = generaSinusoide(freq, sampleRate, step);
+        float[] silenci = generaSilenci(sampleRate, step);
 
-        float[] sineWave = generateSineWave(frequency, sampleRate, durationInSeconds);
-
-        audioDevice.writeSamples(sineWave, 0, sineWave.length);
+        while(running) {
+            if( sona )
+                audioDevice.writeSamples(sineWave, 0, sineWave.length);
+            else {
+                audioDevice.writeSamples(silenci, 0, silenci.length);
+            }
+        }
     });
 
 
@@ -44,27 +57,38 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
 
-        //int durationInSeconds = 5;
-        //float[] sineWave = generateSineWave(freq, sampleRate, durationInSeconds);
-
         // Crear un dispositiu d'àudio i reproduir l'ona sinusoïdal
         audioDevice = Gdx.audio.newAudioDevice((int) sampleRate, true);
-        //audioDevice.writeSamples(sineWave, 0, sineWave.length);
-
+        // posem en marxa el thread d'audio
         soundThread.start();
     }
 
     @Override
     public void render() {
+        // GUI
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
+
+        // al detectar un touch apaguem o engeguem
+        if( Gdx.input.justTouched() ) {
+            sona = ! sona;
+        }
     }
 
     @Override
     public void dispose() {
+        // indiquem al thread de so que acabi
+        running = false;
+        // destruim objectes GUI
         batch.dispose();
         image.dispose();
+        // esperem a que acabi de sortir el thread d'audio
+        try {
+            soundThread.join();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
